54b7dd4ed58535431d4a9b32c34b5a3f9614b858
Rhetor4Mentor <rhethor4mentor@gmail.com>
2025-01-20 19:35:25 -0500

    image paths, check

--- app/components/header.py
+++ app/components/header.py
@@ -1,9 +1,15 @@
 import streamlit as st
+import os
 
 TITLE = "SC Fund"
 HEADER = "SC Funding Analyser"
 
 def get_header(page_title: str = TITLE, header: str = HEADER):
-
     st.title(header)
-    st.logo("app/images/scfund_logo2.png", size="large",)
+    logo_path = os.path.join(os.path.dirname(__file__), "../images/scfund_logo2.png")
+    logo_path = os.path.abspath(logo_path)
+    
+    if os.path.exists(logo_path):
+        st.logo(logo_path, size="large")
+    else:
+        st.warning("Logo image not found. Please ensure the image is available at the specified path.")
