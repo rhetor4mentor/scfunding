277cb4aeb763912a74833a95484cfadc71483a6c
Rhetor4Mentor <rhethor4mentor@gmail.com>
2025-01-20 20:38:14 -0500

    fixed repeated session state initialisation

--- app/data.py
+++ app/data.py
@@ -1,16 +1,16 @@
 import streamlit as st
 from src.loading.loader import TransactionParser
 from src.loading.combined_signals import CompleteTimeSeries
+from loguru import logger
 
-@st.cache_resource
-def load_transaction_parser() -> TransactionParser:
-    return TransactionParser()
 
 @st.cache_resource
 def load_complete_time_series(refresh_counter: int = 0) -> CompleteTimeSeries:
+    logger.info(f"Loading complete time series with refresh_counter={refresh_counter}")
     return CompleteTimeSeries()
 
 def populate_session_state(cts: CompleteTimeSeries) -> None:
+    logger.info("Populating session state with complete time series data")
     st.session_state['cts'] = cts
     st.session_state['ts_daily'] = cts.get_time_series('D')
     st.session_state['ts_weekly'] = cts.get_time_series('W')
@@ -18,7 +18,8 @@ def populate_session_state(cts: CompleteTimeSeries) -> None:
     st.session_state['main_statistics'] = cts.transaction_parser.main_statistics
 
 def initialize_session_state():
-    if 'complete_time_series' not in st.session_state:
+    if 'cts' not in st.session_state:
+        logger.info("Initializing session state")
         cts = load_complete_time_series(refresh_counter=0)
         populate_session_state(cts)
 
@@ -27,5 +28,6 @@ def refresh_session_state():
         st.session_state['refresh_counter'] = 0
 
     st.session_state['refresh_counter'] += 1
+    logger.info(f"Refreshing session state, refresh_counter={st.session_state['refresh_counter']}")
     cts = load_complete_time_series(st.session_state['refresh_counter'])
     populate_session_state(cts)
