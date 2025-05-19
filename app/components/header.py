4dac3ff9121ed6dff1e78651a64f067dae08a19c
Rhetor4Mentor <rhethor4mentor@gmail.com>
2025-05-19 11:17:32 -0400

    updates to topline statistics and header

--- app/components/header.py
+++ app/components/header.py
@@ -1,12 +1,22 @@
 import streamlit as st
 import os
 
+from src.utils import format_timedelta
+from data import refresh_session_state
+
+
 TITLE = "SC Fund"
 HEADER = "SC Funding Analyzer"
 
+metrics = st.session_state["main_statistics"]
+delta_time = format_timedelta(td=metrics["time"]["time_since_measure"])
+
 
 def get_header(page_title: str = TITLE, header: str = HEADER):
-    st.title(header)
+
+    header_left, header_right = st.columns([6, 6])
+    header_left.title(header)
+
     logo_path = os.path.join(os.path.dirname(__file__), "../images/scfund_logo2.png")
     logo_path = os.path.abspath(logo_path)
 
@@ -16,3 +26,15 @@ def get_header(page_title: str = TITLE, header: str = HEADER):
         st.warning(
             "Logo image not found. Please ensure the image is available at the specified path."
         )
+
+    with header_right:
+        refresh_cont = st.container(border=True)
+        left, right = refresh_cont.columns([2, 10])
+
+        with left:
+            if st.button("Refresh"):
+                refresh_session_state()
+
+        with right:
+            st.markdown(f"**Last refresh.**\n{delta_time} ago.", help="Based on the last hourly transaction record captured at last refresh.")            
+    
